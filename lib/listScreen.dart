d29b7a8a8eb041ed7db3a6219364d238683e5a70
Erik Michel <cf18erik.michel@iesjoandaustria.org>
2022-05-22 12:52:44 +0200

    Interficie actualitzada

--- lib/listScreen.dart
+++ lib/listScreen.dart
@@ -45,22 +45,59 @@ class _ListScreenState extends State<ListScreen> {
                                 horizontal: 10, vertical: 5),
                             padding: const EdgeInsets.all(20.0),
                             decoration: BoxDecoration(
-                              color: const Color(0xff97FFFF),
+                              gradient: LinearGradient(
+                                colors: <Color>[Colors.cyan, Colors.red],
+                              ),
                               borderRadius: BorderRadius.circular(15.0),
                             ),
-                            child: Column(
+                            child: Row(
                               mainAxisAlignment: MainAxisAlignment.start,
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                Text(
-                                  snapshot.data![index].title,
-                                  style: const TextStyle(
-                                    fontSize: 18.0,
-                                    fontWeight: FontWeight.bold,
+                                Container(
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.62,
+                                  padding:
+                                      const EdgeInsets.fromLTRB(0, 0, 10, 0),
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        snapshot.data![index].title,
+                                        overflow: TextOverflow.fade,
+                                        softWrap: false,
+                                        maxLines: 1,
+                                        style: const TextStyle(
+                                          fontSize: 18.0,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                      const SizedBox(height: 10),
+                                      Text(snapshot.data![index].explanation,
+                                          overflow: TextOverflow.ellipsis,
+                                          maxLines: 3,
+                                          style: const TextStyle(
+                                            color: Colors.white,
+                                          )),
+                                    ],
                                   ),
                                 ),
-                                const SizedBox(height: 10),
-                                Text(snapshot.data![index].explanation),
+                                Container(
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.21,
+                                  height:
+                                      MediaQuery.of(context).size.width * 0.21,
+                                  decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      borderRadius: BorderRadius.circular(15.0),
+                                      image: DecorationImage(
+                                          image: NetworkImage(
+                                              snapshot.data![index].url),
+                                          fit: BoxFit.cover)),
+                                ),
                               ],
                             ),
                           ),
